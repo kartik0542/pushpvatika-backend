c4d0102fcf1ff15d152a3b6dada4f95ee7506568
Kartik Kathrotiya <kartikkathrotiya0542@gmail.com>
2026-05-19 18:21:06 +0530

    import User model in messages route

--- routes/messages.js
+++ routes/messages.js
@@ -1,6 +1,7 @@
 import express from "express";
 import Message from "../models/Message.js";
 import authMiddleware from "../middleware/auth.js";
+import User from '../models/User.js';
 
 const router = express.Router();
 
